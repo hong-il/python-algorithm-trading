46424343755ca6efd386d0a0c86620ecb56bf9e4
KIM HONGIL <clroyan01@gmail.com>
2021-06-24 23:51:42 +0900

    Slack 메세지 전송 메소드 분할 ['알람', '에러']

--- sendMessage.py
+++ sendMessage.py
@@ -1,22 +1,37 @@
 """
-    @ Author    : hong-il
+    @ Author    : hikim
     @ Date      : 2021-06-19
     @ File name : sendMessage.py
-    @ File path : 
-    @ Description : App Home 에서 App 이름 설정
+    @ File path :
+    @ Description : Slack 메세지 전송 표준화
 """
 import json
 import requests
 
 
 class SendMessage:
-
-    @staticmethod
-    def post_message(channel, text):
+    def __init__(self):
         with open("config.json") as config_file:
-            config = json.load(config_file)
-            token_info = config.get("token")
-        response = requests.post("https://slack.com/api/chat.postMessage",
-                                 headers={"Authorization": "Bearer " + token_info},
-                                 data={"channel": channel, "text": text}
-                                 )
+            token_info = json.load(config_file)
+            self.alarm_token = token_info.get("alarm_token")
+            self.error_token = token_info.get("error_token")
+
+    def alarm_message(self, channel, attachments):
+        data = {
+            'channel': channel,
+            'attachments': attachments
+        }
+        requests.post("https://slack.com/api/chat.postMessage",
+                      headers={"Authorization": "Bearer " + self.alarm_token, 'Content-Type': 'application/json'},
+                      data=json.dumps(data)
+                      )
+
+    def error_message(self, channel, attachments):
+        data = {
+            'channel': channel,
+            'attachments': attachments
+        }
+        requests.post("https://slack.com/api/chat.postMessage",
+                      headers={"Authorization": "Bearer " + self.error_token, 'Content-Type': 'application/json'},
+                      data=json.dumps(data)
+                      )
